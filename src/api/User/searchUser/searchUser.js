3047983cd6a1a65934344944a24926e186df7853
dnjsvltm201311681 <dnjsvltm201311681@gmail.com>
2020-11-29 22:37:03 +0900

    searchUser 유저검색

---
+++ src/api/User/searchUser/searchUser.js
@@ -0,0 +1,17 @@
+//사용자 검색하기
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+    Query: {
+        searchUser: async(_, args) =>
+            prisma.users({
+                where: {
+                    OR: [
+                        {userName_contains: args.term},
+                        {firstName_contains: args.term},
+                        {lastName_contains: args.term}
+                    ]
+                }
+            })
+    }
+}
