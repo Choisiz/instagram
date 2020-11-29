db055453f30d80e61e27954f9c6838a05bb7f751
dnjsvltm201311681 <dnjsvltm201311681@gmail.com>
2020-11-29 22:49:24 +0900

    searchPost 포스트 검색하기

---
+++ src/api/Post/searchPost/searchPost.js
@@ -0,0 +1,16 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+//포스트 검색하기
+export default {
+    Query: {
+        searchPost: async (_,args) =>
+        prisma.posts({
+            where:{
+                OR: [
+                    {location_starts_with: args.term},
+                    {caption_starts_with: args.term}
+                ]
+            }
+        })
+    }
+}
