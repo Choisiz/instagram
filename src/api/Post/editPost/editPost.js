9eab0759711cb339a91648dd17b6e53f9b5a33d1
dnjsvltm201311681 <dnjsvltm201311681@gmail.com>
2020-12-10 22:28:03 +0900

    editpost 수정 & deletepost

--- src/api/Post/editPost/editPost.js
+++ src/api/Post/editPost/editPost.js
@@ -1,10 +1,13 @@
 import { prisma } from "../../../../generated/prisma-client";
 
+const DELETE ="DELETE";
+const EDIT ="EDIT";
+
 export default {
     Mutation: {
         editPost: async(_,args,{request,isAuthenticated})=> {
             isAuthenticated(request);
-            const {id, caption, location} = args;
+            const {id, caption, location,action} = args;
             const {user} =request;
             const post = await prisma.$exists.post({
                 id,
@@ -13,10 +16,14 @@ export default {
                 }
             });
             if(post){
-                return prisma.updatePost({
-                    data:{ caption, location },
-                    where: { id }
-                });
+                if(action === EDIT){
+                    return prisma.updatePost({
+                        data:{ caption, location },
+                        where: { id }
+                    });
+                }else if(action === DELETE){
+                    return prisma.deletePost({id});
+                }
             }else{
                 throw Error("에러입니다.");
             }
