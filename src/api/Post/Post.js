8b7aef402d3fb6f428f4f0d54f8a4e00efd0747b
dnjsvltm201311681 <dnjsvltm201311681@gmail.com>
2020-12-17 09:13:11 +0900

    복잡하고 긴 fragment 를 computed로 변경하기

--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -1,7 +1,10 @@
 import { prisma } from "../../../generated/prisma-client";
 
 export default {
-    Post: {
+    Post: { //fragment 대신 computed 
+        files: ({id}) => prisma.post({id}).files(),
+        comments: ({id}) => prisma.post({id}).comments(),
+        user: ({id}) => prisma.post({id}).user(),
         isLiked: async (parent, _, {request}) => {
             const {user} = request; 
             const {id} = parent; //입력id = postId
--- src/api/Post/seeFullPost/seeFullPost.js
+++ src/api/Post/seeFullPost/seeFullPost.js
@@ -1,12 +1,10 @@
 import { prisma } from "../../../../generated/prisma-client";
-import { COMMENT_FRAGMENT, FULL_POST_FRAGMENT } from "../../../fragment";
 
 export default {
     Query: {
         seeFullPost: async(_, args) => {
             const {id} = args;
-            console.log(id);
-            return prisma.post({id}).$fragment(FULL_POST_FRAGMENT);
+            return prisma.post({id});
         }
     }
 }
