495fee19344fe9a76808f299a71a442c39e78b5c
dnjsvltm201311681 <dnjsvltm201311681@gmail.com>
2020-12-29 21:12:49 +0900

    변경

--- src/api/User/createAcount/createAcount.js
+++ src/api/User/createAcount/createAcount.js
@@ -5,14 +5,18 @@ export default {
     Mutation: {
         createAcount: async(_,args) =>{
             const {userName, email, firstName="", lastName="",bio=""} = args;
-            const user = await prisma.createUser({
-                userName,
-                email,
-                firstName,
-                lastName,
-                bio
-            });
-            return user;
+            const exists = await prisma.$exists.user({userName});
+            if(exists){
+                throw Error("유저가 존재함");
+            }
+                await prisma.createUser({
+                    userName,
+                    email,
+                    firstName,
+                    lastName,
+                    bio
+                });
+                return true;
         }
     }
 }
--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -5,10 +5,10 @@ import {prisma} from "../../../../generated/prisma-client";
 export default {
     Mutation: {
         requestSecret: async(_,args,{request}) =>{
+            console.log(args);
             const {email} =args;
             const loginSecret= generateSecret();
             try{
-                
                 await sendSecretMail(email, loginSecret);
                 await prisma.updateUser({data:{loginSecret}, where:{email}});
                 return true;
